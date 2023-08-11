29e97129fa69d454fcd1b99639379ebde9d13e9c
Bilal cinal <73084109+bilalcinal@users.noreply.github.com>
2023-08-11 15:01:51 +0300

    JWT Token Added

--- Authentication.API/Controllers/AuthenticationController.cs
+++ Authentication.API/Controllers/AuthenticationController.cs
@@ -1,9 +1,15 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
 using Account.API.Data;
 using Authentication.API.Data;
 using Authentication.API.Model;
 using Authentication.API.Security.Hashing;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 
 namespace Authentication.API.Controllers
@@ -12,83 +18,67 @@ namespace Authentication.API.Controllers
     [Route("api/[controller]")]
     public class AuthenticationController : ControllerBase
     {
-        private readonly AuthenticationDbContext _authenticationDbContex;
+        private readonly AuthenticationDbContext _authenticationDbContext;
 
-
-         public AuthenticationController(AuthenticationDbContext dbContext)
+        public AuthenticationController(AuthenticationDbContext dbContext)
         {
-            _authenticationDbContex = dbContext;
-           
+            _authenticationDbContext = dbContext;
         }
 
-
-        // [HttpPost("CreateUser")]
-        // public async Task<IActionResult> CreateUser(UserRegisterModel userRegisterModel)
-        // {
-
-        //     var client = new HttpClient();
-        //     string jsonContent = JsonConvert.SerializeObject(userRegisterModel);
-
-        //     var httpContent = new StringContent(jsonContent, System.Text.Encoding.UTF8, "application/json");
-
-        //     var gatewayBaseUrl = "https://localhost:7244"; // Gateway ana adresi
-        //     var endpoint = "/api/User/CreateUser"; // Endpoint adresi
-
-        //     var apiUrl = $"{gatewayBaseUrl}{endpoint}";
-        //     var response = await client.PostAsync(apiUrl, httpContent);
-        //     //var response = await client.PostAsync("gateway + account create user endpoint adresi", httpContent);
-
-        //     HashingHelper.CreatePasswordHash(userRegisterModel.Password , out var passwordHash, out var passwordSalt);
-
-        //     var UserPassword = new UserPassword
-        //     {
-        //      PasswordHash = passwordHash,
-        //      PasswordSalt = passwordSalt
-        //     };
-
-        //   _authenticationDbContex.Add(UserPassword);
-        //   await _authenticationDbContex.SaveChangesAsync();
-        //   return Ok();
-        // }
-
-        [HttpPost("CreateUser")]
+        [HttpPost]
         public async Task<IActionResult> CreateUser(UserRegisterModel userRegisterModel)
         {
             try
             {
+                // Kullanıcının şifresini hash'leyip veritabanına kaydediyoruz
+                HashingHelper.CreatePasswordHash(userRegisterModel.Password, out var passwordHash, out var passwordSalt);
+
+                var userPassword = new UserPassword
+                {
+                    PasswordHash = passwordHash,
+                    PasswordSalt = passwordSalt
+                };
+
+                _authenticationDbContext.Add(userPassword);
+                await _authenticationDbContext.SaveChangesAsync();
+
+                // JWT üretimi
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key = Encoding.ASCII.GetBytes("3KBsVR697nrsqxfvvjlZDw=="); // Gizli anahtar
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim(ClaimTypes.Name, userRegisterModel.UserName)
+                        // Diğer talepleri buraya ekleyebilirsiniz
+                    }),
+                    Expires = DateTime.UtcNow.AddHours(1), // Token süresi
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                };
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                var tokenString = tokenHandler.WriteToken(token);
+
+                // Kullanıcı kayıt modelini JSON formatına dönüştürüyoruz
+                string jsonContent = JsonConvert.SerializeObject(userRegisterModel);
+
+                // JSON içeriğini HTTP isteği içeriği olarak ayarlıyoruz
+                var httpContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+
+                // Gateway endpoint adresini belirliyoruz
+                var endpoint = "/Authentication/CreateUser";
+
+                // Gateway ana adresi ve endpoint adresini belirliyoruz
+                var gatewayBaseUrl = "https://localhost:7244";
+                var apiUrl = $"{gatewayBaseUrl}{endpoint}";
+
                 using (var client = new HttpClient())
                 {
-                    // Kullanıcı kayıt modelini JSON formatına dönüştürüyoruz
-                    string jsonContent = JsonConvert.SerializeObject(userRegisterModel);
-
-                    // JSON içeriğini HTTP isteği içeriği olarak ayarlıyoruz
-                    var httpContent = new StringContent(jsonContent, System.Text.Encoding.UTF8, "application/json");
-
-                    // Gateway endpoint adresini belirliyoruz
-                    var endpoint = "/Authentication/CreateUser";
-
-                    // Gateway ana adresi ve endpoint adresini belirliyoruz
-                    var gatewayBaseUrl = "https://localhost:7244";
-                    var apiUrl = $"{gatewayBaseUrl}{endpoint}";
-
                     // Gateway üzerinden kullanıcı kayıt isteği atıyoruz
                     var response = await client.PostAsync(apiUrl, httpContent);
                     response.EnsureSuccessStatusCode(); // Hata durumunu kontrol ediyoruz
-
-                    // Kullanıcının şifresini hash'leyip veritabanına kaydediyoruz
-                    HashingHelper.CreatePasswordHash(userRegisterModel.Password, out var passwordHash, out var passwordSalt);
-
-                    var UserPassword = new UserPassword
-                    {
-                        PasswordHash = passwordHash,
-                        PasswordSalt = passwordSalt
-                    };
-
-                    _authenticationDbContex.Add(UserPassword);
-                    await _authenticationDbContex.SaveChangesAsync();
-
-                    return Ok(); // Başarılı ise 200 OK cevabı dönüyoruz
                 }
+
+                return Ok(new { Token = tokenString }); // Başarılı ise token ile 200 OK cevabı dönüyoruz
             }
             catch (HttpRequestException ex)
             {
@@ -102,30 +92,6 @@ namespace Authentication.API.Controllers
             }
         }
 
-
-
-        [HttpPost("UpdateUser")]
-        public async Task<IActionResult> UpdateUser(int id, [FromBody] UserRegisterModel userRegisterModel)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            var existingUser = await _authenticationDbContex.UserPassword.FindAsync(id);
-            if (existingUser == null)
-                return NotFound();
-
-            try
-            {
-                await _authenticationDbContex.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                return StatusCode(500);
-            }
-
-            return Ok(); 
-        }
-
-
+        
     }
 }
--- Authentication.API/Program.cs
+++ Authentication.API/Program.cs
@@ -1,5 +1,46 @@
-﻿using Authentication.API.Data;
+﻿// using System.Text;
+// using Authentication.API.Data;
+// using Microsoft.AspNetCore.Authentication.JwtBearer;
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.IdentityModel.Tokens;
+// using Microsoft.OpenApi.Models;
+
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Add services to the container.
+
+// builder.Services.AddControllers();
+// builder.Services.AddDbContext<AuthenticationDbContext>(options => options.UseSqlServer(
+//     builder.Configuration.GetConnectionString("DefaultConnection")
+// ));
+// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
+
+// builder.Services.AddSwaggerGen();
+
+// var app = builder.Build();
+
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+// app.UseHttpsRedirection();
+// app.UseAuthentication();
+// app.UseAuthorization();
+
+// app.MapControllers();
+
+// app.Run();
+
+using Authentication.API.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,9 +50,33 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AuthenticationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
 ));
+
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false; // Sadece HTTPS üzerinden kabul etmek istiyorsanız true yapın
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["SecretKey"]))
+    };
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Authentication.API", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -19,14 +84,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Authentication.API v1"));
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
-
